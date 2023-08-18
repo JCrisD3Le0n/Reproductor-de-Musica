3ba4fc7039a9416a70682296b2d8db7b17bd440d
Juan De Leon <ds23003@ues.edu.sv>
2023-08-18 19:59:40 +0100

    Segundo proyecto y primer commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿
+namespace ReproductorDeMusica
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.listBoxCanciones = new System.Windows.Forms.ListBox();
+            this.pictureBoxAlbumImagen = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxAlbumImagen)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(7)))), ((int)(((byte)(221)))));
+            this.btnPlay.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnPlay.BackgroundImage")));
+            this.btnPlay.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnPlay.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnPlay.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPlay.Location = new System.Drawing.Point(177, 146);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(40, 40);
+            this.btnPlay.TabIndex = 0;
+            this.btnPlay.UseVisualStyleBackColor = false;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(7)))), ((int)(((byte)(221)))));
+            this.btnPause.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnPause.BackgroundImage")));
+            this.btnPause.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnPause.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnPause.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPause.Location = new System.Drawing.Point(117, 146);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(35, 35);
+            this.btnPause.TabIndex = 1;
+            this.btnPause.UseVisualStyleBackColor = false;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(7)))), ((int)(((byte)(221)))));
+            this.btnStop.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnStop.BackgroundImage")));
+            this.btnStop.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnStop.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnStop.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStop.Location = new System.Drawing.Point(239, 146);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(35, 35);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // listBoxCanciones
+            // 
+            this.listBoxCanciones.BackColor = System.Drawing.Color.Black;
+            this.listBoxCanciones.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.listBoxCanciones.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBoxCanciones.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.listBoxCanciones.FormattingEnabled = true;
+            this.listBoxCanciones.ItemHeight = 15;
+            this.listBoxCanciones.Location = new System.Drawing.Point(20, 31);
+            this.listBoxCanciones.Name = "listBoxCanciones";
+            this.listBoxCanciones.Size = new System.Drawing.Size(396, 94);
+            this.listBoxCanciones.TabIndex = 3;
+            // 
+            // pictureBoxAlbumImagen
+            // 
+            this.pictureBoxAlbumImagen.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBoxAlbumImagen.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBoxAlbumImagen.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxAlbumImagen.Image")));
+            this.pictureBoxAlbumImagen.Location = new System.Drawing.Point(439, 31);
+            this.pictureBoxAlbumImagen.Name = "pictureBoxAlbumImagen";
+            this.pictureBoxAlbumImagen.Size = new System.Drawing.Size(165, 173);
+            this.pictureBoxAlbumImagen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxAlbumImagen.TabIndex = 4;
+            this.pictureBoxAlbumImagen.TabStop = false;
+            this.pictureBoxAlbumImagen.Click += new System.EventHandler(this.pictureBoxAlbumCover_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Location = new System.Drawing.Point(572, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(26, 25);
+            this.button1.TabIndex = 5;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Location = new System.Drawing.Point(604, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(26, 25);
+            this.button2.TabIndex = 6;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(630, 240);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBoxAlbumImagen);
+            this.Controls.Add(this.listBoxCanciones);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.btnPlay);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Reproductor-Musical";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxAlbumImagen)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.ListBox listBoxCanciones;
+        private System.Windows.Forms.PictureBox pictureBoxAlbumImagen;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio.Wave;//Esta biblioteca hace posible el hacer uso de audios
+using NAudio;
+using System.IO;//Biblioteca que permite el manejo, uso de entrada y salida de archivos
+using TagLib;//Biblioteca para trabajar con metadatos de audio
+
+/*---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+namespace ReproductorDeMusica
+{
+    public partial class Form1 : Form
+    {
+        private IWavePlayer cancion;
+        private AudioFileReader audioFile;
+
+        public Form1()
+        {
+            InitializeComponent();
+            cancion = new WaveOutEvent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            // Agrega y carga la lista de canciones dentro de "listBoxCanciones"
+
+            string carpetaDeMusica = @"D:\musica"; //Almacena la ruta de nuestra musica
+            string[] archivosMusica = Directory.GetFiles(carpetaDeMusica, "*.mp3");
+
+            // Agrega el titulo de las canciones
+            foreach (string archivo in archivosMusica)
+            {
+                string nombreArchivo = Path.GetFileNameWithoutExtension(archivo);
+                listBoxCanciones.Items.Add(nombreArchivo);
+            }
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            if (listBoxCanciones.SelectedIndex != -1)
+            {
+                string selectedSong = Path.Combine(@"D:\musica", listBoxCanciones.SelectedItem + ".mp3");
+                audioFile = new AudioFileReader(selectedSong);
+
+                // Detiene y libera la canción anterior (si existe)
+                cancion.Stop();
+                cancion.Dispose();
+                cancion = new WaveOutEvent();
+
+                // Asigna la fuente al reproductor y reproduce
+                cancion.Init(audioFile);
+                cancion.Play();
+
+                // Carga la portada de la canción en el PictureBox
+                LoadAlbumArt(selectedSong);
+            }
+        }
+
+
+        private void LoadAlbumArt(string filePath)
+        {
+            using (TagLib.File tagFile = TagLib.File.Create(filePath))
+            {
+                if (tagFile.Tag.Pictures.Length >= 1)//Al hacer una revision de los metadatos, busca una portada de la cancion que se reproduce
+                {
+                    var albumArtBytes = tagFile.Tag.Pictures[0].Data.Data;
+                    using (MemoryStream ms = new MemoryStream(albumArtBytes))
+                    {
+                        pictureBoxAlbumImagen.Image = new System.Drawing.Bitmap(ms);
+                    }
+                }
+                else
+                {
+                    pictureBoxAlbumImagen.Image = Properties.Resources.alternativa;//Si al reproducir la cancion esta no posee ninguna portada se muestra una imagen por defecto o alternativa
+
+                }
+            }
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            cancion.Pause();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (cancion != null)
+            {
+                cancion.Stop();
+                audioFile.Position = 0; // Se reinicia la posicion de la cancion que se esta reproduciendo desde el inicio
+            }
+        }
+
+        private void pictureBoxAlbumCover_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConfigurarRuta_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();//salir de la aplicacion
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;//Minimizar
+        }
+    }
+    }
+//Desarrollador: Juan De Leon 
+//Fecha de actualizacion: 18/08/2023
+
+
+//Funcionamiento-------------------------------------------------------------------------------------------------------------------
+/*importamos las bibliotecas que seran necesarias para el funcionamiento de la aplicacion, hacemos uso de un
+lector de archivos de audio y uno de reproduccion, un evento que obtiene una lista de los archivos del directorio de musica
+agregando solo los titulos
+el boton "play" contiene un evento y carga el archivo que el usuario selecciona y carga una portada 
+Método LoadAlbumArt: Carga la portada del álbum de la canción actual utilizando la biblioteca TagLib.
+Si no se encuentra una portada, muestra una imagen alternativa.
+*/
+
